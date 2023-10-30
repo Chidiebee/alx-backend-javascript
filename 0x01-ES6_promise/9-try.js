00bdee0c01211ab9ec012baefb4b53430c3a0ebf
Chidiebee <camuchionu3@gmail.com>
2023-10-30 16:55:39 +0100

    Await

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(`Error: ${error.message}`);
-  } finally {
-    queue.push('Guardrail was processed');
+    const queue = [];
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(`Error: ${error.message}`);
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
   }
-  return queue;
-}
